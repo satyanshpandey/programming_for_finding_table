3198cc4794c4e03e8551c4b4cd3a0c867b3b2ab1
satyam <pandeysatyansh2810@gmail.com>
2019-08-18 17:40:40 +0530

    Table programming practice

---
+++ table2.c
@@ -0,0 +1,18 @@
+/*
+In this programming we will be how to find out the table of the number.
+*/
+main()
+{
+	int i,n,num;
+	printf("enter the value of the number:");
+	scanf("%d",&num);
+	for(i=1;i<=10;i++)
+
+{
+	n=i*num;
+
+		
+
+printf("n=%d  %d  \n",i,n);
+}
+} 
